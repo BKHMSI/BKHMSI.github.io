b0889e978d88d8283a9d47658c1559603dd3f435
BKHMSI <badr@khamissi.com>
2016-03-06 22:52:59 +0200

    Scoreboard
    Separate CSS and JS files

---
+++ Projects/Scoreboard/Scoreboard.js
@@ -0,0 +1,124 @@
+var html = "";
+
+$(document).ready( function () {
+    $('#table_id').DataTable();
+ } );
+
+function httpRequest(url){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function getJSON(handle, max){
+  var url = "http://codeforces.com/api/user.status?handle="+handle+"&from=1&count="+max;
+  var json = httpRequest(url);
+  var obj = JSON.parse(json);
+  return obj;
+}
+
+function drawTableHeader(problems){
+  html = "<table id=\"table_id\" class=\"display\"><thead><tr>";
+  html+="<td>Team Handle</td>";
+  for(var i = 0; i<problems.length; i++)
+      html+=("<td>"+problems[i]+"</td>");
+  html+="<td>Score</td>";
+  html+="</tr></thead><tbody>";
+}
+
+function filterProblemsOfUser(problems, handles, max){
+    for(var i = 0; i<handles.length; i++){
+        html+="<tr>";
+        html+=("<td style=\"font-style:bold; height: 50px;\">"+handles[i]+"</td>");
+        var json = getJSON(handles[i], max);
+        for(var j = 0; j<problems.length; j++){
+          var wa = 0, ac = 0;
+          var flag = false;
+          for(var k = 0; k<json.result.length; k++){
+              var problem = json.result[k].problem.name;
+              if(problem == problems[j]){
+                var verdict = json.result[k].verdict;
+                if(verdict != "OK") wa++;
+                else{
+                  ac++;
+                  flag = true;
+                }
+             }
+          }
+          if(flag){
+            html+="<td style=\"background-color:#00e600\"> &#10004 </td>";
+          }else if(wa != 0){
+            html+="<td style=\"background-color:#e63900\"> &#x2718 </td>";
+          }else{
+            html+="<td></td>";
+          }
+        }
+        html+="<td>"+Math.random()*100 + 1 +"</td>";
+        html+="</tr>";
+    }
+  html+="</tbody></table>";
+  $(html).appendTo('#scoreboard');
+  $('#table_id').DataTable();
+}
+
+function getUserProblems(handles, max){
+  var json = getJSON(handles[0], max);
+  var handle = json.result[0].author.members[0].handle;
+  html = "<table id=\"table_id\" class=\"display\"><thead><tr>";
+  html+=("<td>"+handle+" Problems </td>");
+  html+=("<td> Verdict </td>");
+  html+=("<td> Date </td>");
+  html+="</tr></thead><tbody>";
+  for(var k = 0; k<json.result.length; k++){
+      html+="<tr>";
+      var problem = json.result[k].problem.name;
+      var verdict = json.result[k].verdict;
+      var time = json.result[k].creationTimeSeconds;
+      var myDate = new Date( time * 1000);
+      html+="<td>"+problem+"</td>";
+      html+="<td>"+verdict+"</td>";
+      html+="<td>"+myDate.toGMTString()+"</td>";
+      html+="</tr>"
+  }
+  html+="</tbody></table>";
+  $(html).appendTo('#scoreboard');
+  $('#table_id').DataTable();
+}
+
+function run(){
+  html = "";
+  $("#scoreboard").html(html);
+  var usersText = $("#handles").val();
+  var prblmsText = $("#problems").val();
+  var max = $("#count").val();
+  if(isNaN(parseInt(max))){
+    max = 50;
+  }
+  var handles = usersText.split("\n");
+  var problems = prblmsText.split("\n");
+  console.log(problems);
+  if(problems.length == 1){
+      console.log(handles);
+      getUserProblems(handles, parseInt(max));
+  }else{
+    drawTableHeader(problems);
+    filterProblemsOfUser(problems, handles, parseInt(max));
+  }
+}
+
+/*
+DanMaklen
+Dokany
+Bkhmsi
+lightningstruct
+
+Moore's Law
+Lucky Numbers
+War of the Corporations
+Gena's Code
+USB Flash Drives
+Pasha and Stick
+K-special Tables
+
+*/
