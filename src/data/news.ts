4e785de08ab62ff938a05d08f5483e007763602b
bkhmsi <badr@khamissi.com>
2026-07-12 08:30:18 +0200

    Deploy site 2026-07-12 08:30:16

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,7 +7,7 @@ export const newsItems: NewsItem[] = [
   },
   {
     date: '9 July 2026',
-    html: 'The extended abstract of our paper <strong><a href="https://arxiv.org/abs/2606.09770">Discovering Functionally Selective Brain Regions with a Deep Topographic Multimodal Model</a></strong> has been accepted as a <strong>Spotlight (top-10%)</strong> at <strong><a href="https://2026.ccneuro.org">CCN 2026</a></strong>!'
+    html: 'The extended abstract of our paper <strong><a href="https://arxiv.org/abs/2606.09770">Discovering Functionally Selective Brain Regions with a Deep Topographic Multimodal Model</a></strong> has been accepted as a <strong>Spotlight (top 10%)</strong> at <strong><a href="https://2026.ccneuro.org">CCN 2026</a></strong>!'
   },
   {
     date: '8 July 2026',
